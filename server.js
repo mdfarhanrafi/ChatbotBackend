67f4a23f0a425f7efacfe80db96dc35bfd121c4b
mdfarhanrafi <mdfarhanrafi563@gmail.com>
2024-10-01 22:24:39 +0600

    Cors setup

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ const port = process.env.PORT || 5001;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Replace with your domain
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+// Rest of your code
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
 app.use(cors({
     origin: '*'
 }));
