a2de14415b6a2f3d5d7c51317c0ac8beb902f08e
mdfarhanrafi <mdfarhanrafi563@gmail.com>
2024-10-01 22:29:42 +0600

    Cors setup

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ const port = process.env.PORT || 5001;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Replace with your domain
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", "*"); // Replace with your domain
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 // Rest of your code
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
 app.use(cors({
-    origin: '*'
+    origin: "http://localhost:3000"
 }));
 
 import ApiRoutes from "./routes/api.js"
